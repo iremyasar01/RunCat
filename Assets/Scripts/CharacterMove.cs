2f87587debbc08cedb2ef65a8de1b38815202085
iremyasar01 <99986349+iremyasar01@users.noreply.github.com>
2024-04-14 17:52:26 +0300

    MoveLimit

--- Assets/Scripts/CharacterMove.cs
+++ Assets/Scripts/CharacterMove.cs
@@ -7,6 +7,8 @@ public class CharacterMove : MonoBehaviour
 {
     public Animator animator;
     public int RunSpeed =10;
+    public float maxPosition = 3.43f; // Maksimum konum
+    public float minPosition = -3.43f; // Min konum
 
     // Start is called before the first frame update
     void Start()
@@ -33,14 +35,33 @@ public class CharacterMove : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             AnimationControl("Run");
-            
+            //transform.position = new Vector3(-3.43f, transform.position.y, transform.position.z);
+
             transform.Translate(Vector3.left * Time.deltaTime * RunSpeed);
+            // Karakterin mevcut konumunu kontrol edelim
+            if (transform.position.x < minPosition)
+            {
+                // Eğer karakterin konumu maksimum konumdan büyükse, karakteri maksimum konumda tutalım
+                Vector3 newPosition = transform.position;
+                newPosition.x = minPosition;
+                transform.position = newPosition;
+            }
 
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
             AnimationControl("Run");
+            //aşağıdaki gibi olunca direkt 3.43 x konumuna gidiyor.
+            //transform.position = new Vector3(3.43f, transform.position.y, transform.position.z);
             transform.Translate(Vector3.right * Time.deltaTime * RunSpeed);
+            // Karakterin mevcut konumunu kontrol edelim
+            if (transform.position.x > maxPosition)
+            {
+                // Eğer karakterin konumu maksimum konumdan büyükse, karakteri maksimum konumda tutalım
+                Vector3 newPosition = transform.position;
+                newPosition.x = maxPosition;
+                transform.position = newPosition;
+            }
 
         }
         //bi kere çalışsın diye.
